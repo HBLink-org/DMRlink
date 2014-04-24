3e8177f80b1b7f62dc1301ac4b64f43c04662fbd
Cort Buffington <n0mjs@me.com>
2014-04-24 08:18:57 -0500

    Cleanup

--- playback.py
+++ playback.py
@@ -20,13 +20,13 @@ import time
 from dmrlink import IPSC, UnauthIPSC, NETWORK, networks, logger, dmr_nat, int_id, send_to_ipsc
 
 __author__ = 'Cortney T. Buffington, N0MJS'
-__copyright__ = 'Copyright (c) 2013 Cortney T. Buffington, N0MJS and the K0USY Group'
-__credits__ = 'Adam Fast, KC0YLK, Dave K, and he who wishes not to be named'
+__copyright__ = 'Copyright (c) 2014 Cortney T. Buffington, N0MJS and the K0USY Group'
+__credits__ = 'Adam Fast, KC0YLK; Dave K; and he who wishes not to be named'
 __license__ = 'Creative Commons Attribution-ShareAlike 3.0 Unported'
-__version__ = '0.2a'
+__version__ = '0.1a'
 __maintainer__ = 'Cort Buffington, N0MJS'
 __email__ = 'n0mjs@me.com'
-__status__ = 'Production'
+__status__ = 'pre-alpha'
 
 class playbackIPSC(IPSC):
     
