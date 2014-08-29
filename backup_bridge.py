92ff27e04ae070d6edc0b81c6c6dbddf8d59edae
Cort Buffington <n0mjs@me.com>
2014-08-29 16:35:58 -0500

    Beta - Currently in Use!

--- backup_bridge.py
+++ backup_bridge.py
@@ -12,19 +12,18 @@ from __future__ import print_function
 from twisted.internet import reactor
 from twisted.internet import task
 from binascii import b2a_hex as h
-import pprint
 
 import sys
 from dmrlink import IPSC, NETWORK, networks, send_to_ipsc, dmr_nat, logger, hex_str_4, int_id
 
 __author__ = 'Cortney T. Buffington, N0MJS'
-__copyright__ = 'Copyright (c) 2013, 2014 Cortney T. Buffington, N0MJS and the K0USY Group'
+__copyright__ = 'Copyright (c) 2014 Cortney T. Buffington, N0MJS and the K0USY Group'
 __credits__ = 'Adam Fast, KC0YLK, Dave K, and he who wishes not to be named'
 __license__ = 'Creative Commons Attribution-ShareAlike 3.0 Unported'
-__version__ = '0.2a'
+__version__ = '0.1b'
 __maintainer__ = 'Cort Buffington, N0MJS'
 __email__ = 'n0mjs@me.com'
-__status__ = 'Production'
+__status__ = 'Beta'
 
 
 # Import Bridging rules
