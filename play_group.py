342f5ac07978ad19e49abf117ac72efb0c354c82
Cort Buffington <n0mjs@me.com>
2015-08-12 10:23:31 -0500

    testing

--- play_group.py
+++ play_group.py
@@ -68,12 +68,17 @@ class playIPSC(IPSC):
     #
     def group_voice(self, _network, _src_sub, _dst_group, _ts, _end, _peerid, _data):
         if _end:
+            '''
             if trigger == False:
                 if (_ts == 0 and _dst_group not in trigger_groups_1) or (_ts == 1 and _dst_group not in trigger_groups_2):
                     return
             else:
                 if (_ts == 0 and _dst_group in trigger_groups_1) or (_ts == 1 and _dst_group in trigger_groups_2):
                     return
+            '''
+            
+            if (_ts == 0 and _dst_group in trigger_groups_1) or (_ts == 1 and _dst_group in trigger_groups_2):
+                return
             
             logger.info('(Event ID: %s) Playback triggered from TS %s, TGID %s', self.event_id, (_ts +1), int_id(_dst_group))
             
