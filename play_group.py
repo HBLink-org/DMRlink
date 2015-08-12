7a4e5fd07da3d8de62c01edd8075ca38668532da
Cort Buffington <n0mjs@me.com>
2015-08-12 09:10:50 -0500

    debugging don't use

--- play_group.py
+++ play_group.py
@@ -77,6 +77,8 @@ class playIPSC(IPSC):
                     
                     
                     # re-Write the source subscriber ID to that of this program
+                    print('source:', h(_src_sub))
+                    print('dest:', h(NETWORK[_network]['LOCAL']['RADIO_ID']))
                     _tmp_data = _tmp_data.replace(_src_sub, NETWORK[_network]['LOCAL']['RADIO_ID'])
                     
                     # Re-Write IPSC timeslot value
