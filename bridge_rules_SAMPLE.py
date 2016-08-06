43e2411f3ec9b6ae11bc8ffdbf5889aa5a2c8772
Cort Buffington <n0mjs@me.com>
2016-08-06 11:10:37 -0500

    Update for timers

--- bridge_rules_SAMPLE.py
+++ bridge_rules_SAMPLE.py
@@ -26,13 +26,21 @@ NOTES:
     * NAME is any name you want, and is used to match reciprocal rules for user-activateion
     * ACTIVE should be set to True if you want the rule active by default, False to be inactive
     * ON and OFF are LISTS of Talkgroup IDs used to trigger this rule off and on. Even if you
-        only want one (as shown in the ON example), it has to be in list format.
+        only want one (as shown in the ON example), it has to be in list format. None can be
+        handled with an empty list, such as " 'ON': [] ".
     * TO_TYPE is timeout type. If you want to use timers, ON means when it's turned on, it will
         turn off afer the timout period and OFF means it will turn back on after the timout
         period. If you don't want to use timers, set it to anything else, but 'NONE' might be
         a good value for documentation!
     * TIMOUT is a value in minutes for the timout timer. No, I won't make it 'seconds', so don't
         ask. Timers are performance "expense".
+
+DO YOU THINK THIS FILE IS TOO COMPLICATED?
+    Because you guys all want more and more features, this file is getting complicated. I have
+    dabbled with using a parser to make it easier to build. I'm torn. There is a HUGE benefit
+    to having it like it is. This is a python file. Simply running it
+    (i.e. "python bridge_rules.py) will tell you if there's a syntax error and where. Think
+    about that for a few minutes :)
 '''
 
 RULES = {
